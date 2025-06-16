285049aea588c59b65ceb163f611c596eb0fafa8
lapit017 <68219010017@htc.ac.th>
2025-06-16 13:24:01 +0700

    Add files via upload

---
+++ 666.php
@@ -0,0 +1,13 @@
+<?php
+ echo "Hello word <br>";
+ echo "Hello PHP Programming<br>" ;
+ echo "Pemika Programming";
+?>
+<html>
+<body>
+
+<p>This is a paragraph
+<p>This is a paragraph
+
+</body>
+</html>
